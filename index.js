b75a0eb0231a9f185aa3493ec7213956c2cb7ea2
Matt Garrett <mattga@gmail.com>
2023-08-09 12:12:57 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('ok')
+})
+
+app.get('/throw1', function (req, res) {
+  throw new Error('boooom');
+  res.send('throw error before res.send() call')
+})
+
+app.get('/throw2', function (req, res) {
+  res.send('throw error after res.send() call')
+  throw new Error('boooom');
+})
+
+app.get('/throw3', function (req, res) {
+  res.send('This is a basic Example for Express.js by TUTORIALKART')
+  res.cookie('chocolate', 'chip')
+})
+
+app.listen(8000, function() { console.log('running on port 8000') })
